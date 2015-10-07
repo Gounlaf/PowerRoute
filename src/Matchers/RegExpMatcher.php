93502f9acf0bd18a21ef6fca5051e8e82bf9d711
Mariano Custiel <jmcustiel@gmail.com>
2015-10-07 20:53:14 +0200

    Added phing build file, added dev-dependencies to composer and tests for all execution paths of UrlEvaluator

--- src/Matchers/RegExpMatcher.php
+++ src/Matchers/RegExpMatcher.php
@@ -3,7 +3,7 @@ namespace Mcustiel\PowerRoute\Matchers;
 
 use Mcustiel\PowerRoute\Common\AbstractArgumentAware;
 
-class NotNullMatcher extends AbstractArgumentAware implements MatcherInterface
+class RegExpMatcher extends AbstractArgumentAware implements MatcherInterface
 {
     public function match($value)
     {
--- tests/unit/Evaluators/UrlEvaluatorTest.php
+++ tests/unit/Evaluators/UrlEvaluatorTest.php
@@ -1,12 +1,14 @@
 <?php
 namespace Mcustiel\PowerRoute\Tests\Evaluators;
 
-use Mcustiel\PowerRoute\Evaluators\QueryStringParamEvaluator;
 use Psr\Http\Message\UriInterface;
 use Mcustiel\PowerRoute\Evaluators\UrlEvaluator;
 
 class UrlEvaluatorTest extends AbstractEvaluatorTest
 {
+    /**
+     * @var \Psr\Http\Message\UriInterface|\PHPUnit_Framework_MockObject_MockObject $uri
+     */
     private $uri;
 
     public function setUp()
@@ -20,21 +22,94 @@ class UrlEvaluatorTest extends AbstractEvaluatorTest
     /**
      * @test
      */
-    public function shouldGetTheUriPartRequestedAndPassItToMatcher()
+    public function shouldGetTheFullUriAndPassItToMatcher()
     {
-        $this->request
-            ->expects($this->once())
-            ->method('getUri')
-            ->willReturn($this->uri);
-        $this->uri
-            ->expects($this->once())
-            ->method('__toString')
-            ->willReturn('http://www.example.com/?potato=banana#coconut');
+        $this->prepareUri('__toString', 'http://www.example.com/?potato=banana#coconut');
         $this->matcher
             ->expects($this->once())
             ->method('match')
             ->with($this->equalTo('http://www.example.com/?potato=banana#coconut'));
         $evaluator = new UrlEvaluator('full');
+        $evaluator->evaluate($this->matcher, $this->request);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldGetTheHostPart()
+    {
+        $this->prepareUri('getHost', 'www.example.com');
+        $evaluator = new UrlEvaluator('host');
+        $evaluator->evaluate($this->matcher, $this->request);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldGetTheSchemePart()
+    {
+        $this->prepareUri('getScheme', 'http');
+        $evaluator = new UrlEvaluator('scheme');
+        $evaluator->evaluate($this->matcher, $this->request);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldGetTheAuthorityPart()
+    {
+        $this->prepareUri('getAuthority', 'potato@www.example.com:8080');
+        $evaluator = new UrlEvaluator('authority');
+        $evaluator->evaluate($this->matcher, $this->request);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldGetTheFragmentPart()
+    {
+        $this->prepareUri('getFragment', 'fragment');
+        $evaluator = new UrlEvaluator('fragment');
+        $evaluator->evaluate($this->matcher, $this->request);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldGetThePathPart()
+    {
+        $this->prepareUri('getPath', '/potato');
+        $evaluator = new UrlEvaluator('path');
+        $evaluator->evaluate($this->matcher, $this->request);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldGetThePortPart()
+    {
+        $this->prepareUri('getPort', '8080');
+        $evaluator = new UrlEvaluator('port');
+        $evaluator->evaluate($this->matcher, $this->request);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldGetTheQueryPart()
+    {
+        $this->prepareUri('getQuery', 'potato=banana');
+        $evaluator = new UrlEvaluator('query');
+        $evaluator->evaluate($this->matcher, $this->request);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldGetTheUserInfoPart()
+    {
+        $this->prepareUri('getUserInfo', 'user:pass');
+        $evaluator = new UrlEvaluator('user-info');
         $evaluator->evaluate($this->matcher, $this->request);
     }
 
@@ -54,14 +129,7 @@ class UrlEvaluatorTest extends AbstractEvaluatorTest
      */
     public function shouldReturnSameValueAsMatcher()
     {
-        $this->request
-            ->expects($this->once())
-            ->method('getUri')
-            ->willReturn($this->uri);
-        $this->uri
-            ->expects($this->once())
-            ->method('__toString')
-            ->willReturn('http://www.example.com/?potato=banana#coconut');
+        $this->prepareUri('__toString', 'http://www.example.com/?potato=banana#coconut');
         $this->matcher
             ->expects($this->once())
             ->method('match')
@@ -69,5 +137,17 @@ class UrlEvaluatorTest extends AbstractEvaluatorTest
             ->willReturn(true);
         $evaluator = new UrlEvaluator('full');
         $this->assertTrue($evaluator->evaluate($this->matcher, $this->request));
+    }
+
+    private function prepareUri($method, $returnValue)
+    {
+        $this->request
+            ->expects($this->once())
+            ->method('getUri')
+            ->willReturn($this->uri);
+        $this->uri
+            ->expects($this->once())
+            ->method($method)
+            ->willReturn($returnValue);
     }
 }
