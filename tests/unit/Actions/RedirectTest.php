d3f0bea20b1ab8a06e4de07506950cde7afe85c5
Mariano Custiel <jmcustiel@gmail.com>
2015-11-11 18:57:51 +0100

    Added tests for Redirect action

---
+++ tests/unit/Actions/RedirectTest.php
@@ -0,0 +1,61 @@
+<?php
+namespace Mcustiel\PowerRoute\Tests\InputSources;
+
+use Mcustiel\PowerRoute\Common\TransactionData;
+use Zend\Diactoros\ServerRequest;
+use Zend\Diactoros\Response;
+use Mcustiel\PowerRoute\Actions\ActionInterface;
+use Mcustiel\PowerRoute\Actions\Redirect;
+
+class RedirectTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var \Mcustiel\PowerRoute\Actions\ActionInterface
+     */
+    private $action;
+
+    /**
+     * @before
+     */
+    public function initAction()
+    {
+        $this->action = new Redirect('http://github.com');
+    }
+
+    /**
+     * @test
+     */
+    public function shouldSetARedirectResponse()
+    {
+        $transaction = new TransactionData(new ServerRequest(), new Response());
+        $this->action->execute($transaction);
+        $this->assertEquals(302, $transaction->getResponse()->getStatusCode());
+        $this->assertArrayHasKey('Location', $transaction->getResponse()->getHeaders());
+        $this->assertEquals(
+            'http://github.com',
+            $transaction->getResponse()->getHeaderLine('Location')
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function shouldSetARedirecResponseAndKeepOldDataFromResponse()
+    {
+        $transaction = new TransactionData(
+            new ServerRequest(),
+            new Response('data://text/plain,This is the previous text')
+        );
+        $this->action->execute($transaction);
+        $this->assertEquals(302, $transaction->getResponse()->getStatusCode());
+        $this->assertArrayHasKey('Location', $transaction->getResponse()->getHeaders());
+        $this->assertEquals(
+            'http://github.com',
+            $transaction->getResponse()->getHeaderLine('Location')
+        );
+        $this->assertEquals(
+            'This is the previous text',
+            $transaction->getResponse()->getBody()->__toString()
+        );
+    }
+}
